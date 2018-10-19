85b3cc743e93db1ad3826abd9a400840df06a0b9
Lê Đình Cường <Ledinhcuong@.github.com>
2018-10-19 23:48:57 +0700

    search view

--- search.php
+++ search.php
@@ -85,6 +85,10 @@
 
 		<div class="search">
 			<div class="container">
+				<div class="key-search">
+					Kết tìm kiếm cho "Dailly"
+				</div>
+
 				<div class="content-search">
 					<div class="total-result">
 						Có tổng cộng 10 kết quả trả về
@@ -96,20 +100,108 @@
 				<div class="row">
 					<div class="col-md-3 app-result">
 						<a href="#">
-							
-							<div class="image-app">
-								<img class="imge-resize" src="public/images/datallyicon.png" alt="">
+							<div class="background-result">
+
+								<div class="image-app">
+									<img class="imge-resize" src="public/images/datallyicon.png" alt="">
+								</div>
+
+								<span class="name-app-search">
+									Dailly
+								</span>
+								<div class="name-company">
+									Google
+								</div>
+								
 							</div>
-
-							<span class="name-app-search">
-								Dailly
-							</span>
+							
+							
 						</a>
-						
-
 					</div>
+					<div class="col-md-3 app-result">
+						<a href="#">
+							<div class="background-result">
+
+								<div class="image-app">
+									<img class="imge-resize" src="public/images/datallyicon.png" alt="">
+								</div>
+
+								<span class="name-app-search">
+									Dailly
+								</span>
+								<div class="name-company">
+									Google
+								</div>
+								
+							</div>
+							
+							
+						</a>
+					</div>
+					<div class="col-md-3 app-result">
+						<a href="#">
+							<div class="background-result">
+
+								<div class="image-app">
+									<img class="imge-resize" src="public/images/datallyicon.png" alt="">
+								</div>
+
+								<span class="name-app-search">
+									Dailly
+								</span>
+								<div class="name-company">
+									Google
+								</div>
+								
+							</div>
+							
+							
+						</a>
+					</div>
+					<div class="col-md-3 app-result">
+						<a href="#">
+							<div class="background-result">
+
+								<div class="image-app">
+									<img class="imge-resize" src="public/images/datallyicon.png" alt="">
+								</div>
+
+								<span class="name-app-search">
+									Dailly
+								</span>
+								<div class="name-company">
+									Google
+								</div>
+								
+							</div>
+							
+							
+						</a>
+					</div>
+					<div class="col-md-3 app-result">
+						<a href="#">
+							<div class="background-result">
+
+								<div class="image-app">
+									<img class="imge-resize" src="public/images/datallyicon.png" alt="">
+								</div>
+
+								<span class="name-app-search">
+									Dailly
+								</span>
+								<div class="name-company">
+									Google
+								</div>
+								
+							</div>
+							
+							
+						</a>
+					</div>
+					
+
 				</div>
-	
+
 
 			</div>
 		</div>
