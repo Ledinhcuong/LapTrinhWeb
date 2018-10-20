f81487e053be4c3bfc3e39422d03e1ebb1e83e44
Lê Đình Cường <Ledinhcuong@.github.com>
2018-10-20 15:17:38 +0700

    Add laravel

---
+++ public/public/js/sitecripts.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+
+	getDay();
+	
+	$(".float-button").click(function() {
+		
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) $('.float-button').fadeIn();
+			else $('#goTop').fadeOut();
+		});
+		$('.float-button').click(function () {
+			$('body,html').animate({scrollTop: 0}, 'slow');
+		});
+
+	});
+
+	$(".account").click(function() {
+		$('.menu-account').slideToggle(500);
+
+	});
+
+
+	
+	
+});
+
+function showImage() {
+	
+}
+
+function getDay()
+{
+	var day = new Date();
+	var t = day.getDay();
+	
+	var thu = ["Chủ nhật", "Thứ hai", "Thứ ba", "Thứ tư", "Thứ năm", "Thứ sáu", "Thứ bảy"];
+	
+	var today = day.getDate() + "/" + day.getMonth() + "/" + day.getFullYear();
+	
+
+	$('.thu').html(thu[Number(t)]);
+	$('.date').html(today);
+
+	var hour = day.getHours();
+	if (hour >= 5 && hour < 12)
+	{
+		$('.loi-chao').html("Chào buổi sáng !");
+	}
+	else if (hour >= 12 && hour < 18)
+	{
+		$('.loi-chao').html("Chúc bạn buổi chiều tốt lành !");
+	}
+	else
+	{
+		$('.loi-chao').html("Chúc bạn có buổi tối vui vẻ !");
+	}
+}
+
