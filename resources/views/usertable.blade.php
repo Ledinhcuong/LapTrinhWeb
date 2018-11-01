27e4e865925987f2726b3ce9e776e908d124b8a4
Lê Đình Cường <Ledinhcuong@.github.com>
2018-11-01 19:49:19 +0700

    up

--- resources/views/usertable.blade.php
+++ resources/views/usertable.blade.php
@@ -145,6 +145,7 @@
               <th>Id User</th>
               <th>Name User</th>
               <th>Email</th>
+              <th>Control</th>
 
             </tr>
           </thead>
@@ -153,26 +154,71 @@
               <td>1</td>
               <td>Cuong Dinh</td>
               <td>Cuong dinh@gmail.com</td>
+              <td>
+                <div>
+                   <a href="#" style="background: #00c853;" class="btn-control"> <i class="fa fa-pencil-square" aria-hidden="true"></i> Sửa</a>
+                </div>
+                <div>
+                  <a href="#" style="background: #ff3d00;" class="btn-control"> <i class="fa fa-trash-o" aria-hidden="true"></i> Xóa </a>
+                </div>
+                  
+              </td>
             </tr>
             <tr>
               <td>1</td>
               <td>Cuong Dinh</td>
               <td>Cuong dinh@gmail.com</td>
+               <td>
+                <div>
+                   <a href="#" style="background: #00c853;" class="btn-control"> <i class="fa fa-pencil-square" aria-hidden="true"></i> Sửa</a>
+                </div>
+                <div>
+                  <a href="#" style="background: #ff3d00;" class="btn-control"> <i class="fa fa-trash-o" aria-hidden="true"></i> Xóa </a>
+                </div>
+                  
+              </td>
             </tr>
             <tr>
               <td>1</td>
               <td>Cuong Dinh</td>
               <td>Cuong dinh@gmail.com</td>
+               <td>
+                <div>
+                   <a href="#" style="background: #00c853;" class="btn-control"> <i class="fa fa-pencil-square" aria-hidden="true"></i> Sửa</a>
+                </div>
+                <div>
+                  <a href="#" style="background: #ff3d00;" class="btn-control"> <i class="fa fa-trash-o" aria-hidden="true"></i> Xóa </a>
+                </div>
+                  
+              </td>
             </tr>
             <tr>
               <td>1</td>
               <td>Cuong Dinh</td>
               <td>Cuong dinh@gmail.com</td>
+               <td>
+                <div>
+                   <a href="#" style="background: #00c853;" class="btn-control"> <i class="fa fa-pencil-square" aria-hidden="true"></i> Sửa</a>
+                </div>
+                <div>
+                  <a href="#" style="background: #ff3d00;" class="btn-control"> <i class="fa fa-trash-o" aria-hidden="true"></i> Xóa </a>
+                </div>
+                  
+              </td>
             </tr>
             <tr>
               <td>1</td>
               <td>Cuong Dinh</td>
               <td>Cuong dinh@gmail.com</td>
+               <td>
+                <div>
+                   <a href="#" style="background: #00c853;" class="btn-control"> <i class="fa fa-pencil-square" aria-hidden="true"></i> Sửa</a>
+                </div>
+                <div>
+                  <a href="#" style="background: #ff3d00;" class="btn-control"> <i class="fa fa-trash-o" aria-hidden="true"></i> Xóa </a>
+                </div>
+                  
+              </td>
             </tr>
           </tbody>
         </table>
