fbeee944a4aae547aa8fa88943e1d27f1e0cf15f
phuongtranitnl <38458334+phuongtranitnl@users.noreply.github.com>
2018-11-18 18:00:36 +0700

    Cập nhật file login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,70 @@
-@extends('app')
 
-@section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Login</div>
-				<div class="panel-body">
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Software</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<LINK REL="SHORTCUT ICON" href="../public/images/mylogo.ico">
+	<link rel="stylesheet" href="../public/font/OpenSansCondensed-Light.ttf">
+	<link rel="stylesheet" href="../public/bootstrap-3.3.7-dist/css/bootstrap.min.css"> 
+	<link rel="stylesheet" href="../public/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="../public/css/styles.css">
+	<link rel="stylesheet" type="text/css" href="../public/css/slider.css">
+	
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="../public/vendor/animate/animate.css">
+	<!--===============================================================================================-->	
+		<link rel="stylesheet" type="text/css" href="../public/vendor/css-hamburgers/hamburgers.min.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="../public/vendor/select2/select2.min.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="../public/css/util.css">
+		<link rel="stylesheet" type="text/css" href="../public/css/main.css">
+	<!--===============================================================================================-->
+</head>
+<body>
+	<!-- Header -->
+	<header>
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="index"><img src="../public/images/mylogo.ico" alt="">
+						<b>Software</b>
+					</a>
+
+				</div>
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				
+			</div>
+		</nav>
+	</header>
+
+	<!-- Blance -->
+		<div class="blance" style="padding: 30px 0; position: static;" > </div>
+
+	<div class="limiter">
+		<div class="container-login100">
+
+			<div class="wrap-login100">
+				<div class="login100-pic js-tilt" data-tilt>
+					<img src="../public/images/img-01.png" alt="IMG">
+				</div>
+
+				<form class="login100-form validate-form" method="POST" action="/auth/login">
+					<span class="login100-form-title">
+						Member Login
+					</span>
+					
 					@if (count($errors) > 0)
 						<div class="alert alert-danger">
 							<strong>Whoops!</strong> There were some problems with your input.<br><br>
@@ -17,47 +75,135 @@
 							</ul>
 						</div>
 					@endif
+	
+					<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-					<form class="form-horizontal" role="form" method="POST" action="/auth/login">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="email" name="email" placeholder="Email" value="{{ old('email') }}">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+						<input class="input100" type="password" name="password" placeholder="Password">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+					
+					<div class="form-group">
+						<div class="col-md-8 col-md-offset-2">
+							<div class="checkbox">
+								<label>
+									<input type="checkbox" name="remember"> Remember Me
+								</label>
 							</div>
 						</div>
+					</div>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
+					<div class="container-login100-form-btn">
+						<button type="submit" class="login100-form-btn">
+							Login
+						</button>
+					</div>
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<div class="checkbox">
-									<label>
-										<input type="checkbox" name="remember"> Remember Me
-									</label>
-								</div>
-							</div>
-						</div>
+					<div class="text-center p-t-12">
+						<span class="txt1">
+							Forgot
+						</span>
+						<a class="txt2" href="reset">
+							Username / Password?
+						</a>
+					</div>
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary" style="margin-right: 15px;">
-									Login
-								</button>
-
-								<a href="/password/email">Forgot Your Password?</a>
-							</div>
-						</div>
-					</form>
-				</div>
+					<div class="text-center p-t-136">
+						<a class="txt2" href="register">
+							Create your Account
+							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+						</a>
+					</div>
+				</form>
 			</div>
 		</div>
 	</div>
-</div>
-@endsection
+		
+
+	<!-- footer -->
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="title-footer">
+						About Us
+					</div>
+
+					<ul style="list-style: none; padding-left: 0px">
+						<li><a href="#">Thông tin về chúng tôi</a></li>
+						<li><a href="#">Bản quyền</a></li>
+						<li><a href="#">Tin tức công ty</a></li>
+
+					</ul>
+
+				</div>
+
+				<div class="col-md-4">
+					<div class="title-footer">
+						Thông tin liên hệ
+					</div>
+
+					<ul style="list-style: none; padding-left: 0px">
+						<li><a href="#">Email: </a></li>
+						<li><a href="#">SĐT:</a></li>
+						<li><a href="#">Facebook: </a></li>
+					</ul>
+
+				</div>
+
+				<div class="col-md-4">
+					<div class="title-footer">
+						Trang web
+					</div>
+
+					<ul style="list-style: none; padding-left: 0px">
+						<li><a href="#">Báo cáo lỗi </a></li>
+						<li><a href="#">Hỗ trợ</a></li>
+						<li><a href="#">Hồ sơ tài khoản </a></li>
+					</ul>
+
+				</div>
+
+			</div>
+		</div>
+	</footer>
+
+	<script type="text/javascript" src="../public/js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="../public/js/sitecripts.js" ></script>
+	<script type="text/javascript" src="../public/js/slider.js"></script>
+	<script>
+        var indexCurrent = 1;      // Chỉ số hình đầu tiên hiển thị ở slide
+        var loop = true;  // Bật lặp slide 
+        var showbutton =true;  // Hiện 2 button điều hướng
+        var duration = 4000;   // Thời gian chờ chuyển hình (tính theo đơn vị milisecond)
+
+        initSlider();
+    </script>
+    
+	<!--===============================================================================================-->
+		<script src="../public/vendor/bootstrap/js/popper.js"></script>
+		<script src="../public/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<!--===============================================================================================-->
+		<script src="../public/vendor/select2/select2.min.js"></script>
+	<!--===============================================================================================-->
+		<script src="../public/vendor/tilt/tilt.jquery.min.js"></script>
+		<script >
+			$('.js-tilt').tilt({
+				scale: 1.1
+			})
+		</script>
+	<!--===============================================================================================-->
+		<script src="../public/js/main.js"></script>
+</body>
+</html>
