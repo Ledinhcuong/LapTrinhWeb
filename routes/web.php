4f23a1872fbad91fcfa7a5375141349fbf6333b7
Nguyệt <tranthikimnguyet2016@gmail.com>
2018-11-25 21:31:28 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -134,7 +134,7 @@ Route::patch('admin/user',['as' => 'admin.user','uses' => 'UserController@index'
 
 
 
-//=============== trang dang nhap
+//=============== trang dang nhap====================================
 
 
 Route::get('login','LoginController@getLogin');
