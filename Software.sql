95308961940beb873370dcaf65d1f266e8db33e9
phuongtranitnl <38458334+phuongtranitnl@users.noreply.github.com>
2018-12-10 15:59:16 +0700

    Update Software.sql

--- Software.sql
+++ Software.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 07, 2018 at 02:58 PM
+-- Generation Time: Dec 10, 2018 at 08:56 AM
 -- Server version: 5.6.37
 -- PHP Version: 7.1.8
 
@@ -42,7 +42,7 @@ CREATE TABLE IF NOT EXISTS `Application` (
   `Version` double(8,2) NOT NULL,
   `Size` double(8,2) NOT NULL,
   `SortDescription` varchar(255) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `Application`
@@ -50,18 +50,14 @@ CREATE TABLE IF NOT EXISTS `Application` (
 
 INSERT INTO `Application` (`IdCategory`, `IdType`, `IdApplication`, `NameApp`, `Developer`, `Description`, `Icon`, `Image1`, `Image2`, `Image3`, `LinkDownload`, `NumberDownload`, `Version`, `Size`, `SortDescription`) VALUES
 (1, 1, 1, 'Microsoft edge', 'Microsoft', 'Get rid of your navigation bar and learn to appreciate the benefits of gesture-based navigation. There was never before, a more natural way, to control your smartphone. Let the future begin!', 'edgeicon.png', 'edge1.jpg', 'edge2.jpg', 'edge3.jpg', 'https://www.microsoft.com/vi-vn/p/facebook/9wzdncrfj2wl?cid=msft_web_appsforwindows_chart#activetab=pivot:overviewtab', 5, 1.00, 50.00, 'Lướt web nhanh hơn bao giờ hết'),
-(1, 1, 2, 'Gesture', 'Oval', 'Get rid of your navigation bar and learn to appreciate the benefits of gesture-based navigation. There was never before, a more natural way, to control your smartphone. Let the future begin!', 'gestureicon.png', 'gesture1.png', 'gesture2.png', 'gesture3.png', 'https://www.microsoft.com/vi-vn/p/facebook/9wzdncrfj2wl?cid=msft_web_appsforwindows_chart#activetab=pivot:overviewtab', 10, 1.00, 50.00, 'Điều hướng phong cách iphone x'),
-(1, 6, 3, 'Volume slide', 'Maxtine', 'Get rid of your navigation bar and learn to appreciate the benefits of gesture-based navigation. There was never before, a more natural way, to control your smartphone. Let the future begin!', 'volumeslideicon.png', 'volumeslide1.png', 'volumeslide2.png', 'volumeslide3.png', 'https://www.microsoft.com/vi-vn/p/facebook/9wzdncrfj2wl?cid=msft_web_appsforwindows_chart#activetab=pivot:overviewtab', 80, 1.00, 50.00, 'Điều chỉnh âm lượng phong cách android 9'),
 (1, 1, 4, 'Stick share', 'the verb', 'Get rid of your navigation bar and learn to appreciate the benefits of gesture-based navigation. There was never before, a more natural way, to control your smartphone. Let the future begin!', 'stickshareicon.png', 'stickshare1.png', 'stickshare2.png', 'stickshare3.png', 'https://www.microsoft.com/vi-vn/p/facebook/9wzdncrfj2wl?cid=msft_web_appsforwindows_chart#activetab=pivot:overviewtab', 60, 1.00, 50.00, 'Chụp nhanh màn hình'),
 (1, 6, 5, 'Recent bar', 'apple', 'Get rid of your navigation bar and learn to appreciate the benefits of gesture-based navigation. There was never before, a more natural way, to control your smartphone. Let the future begin!', 'recentbaricon.png', 'recentbar1.png', 'recentbar2.png', 'recentbar3.png', 'https://www.microsoft.com/vi-vn/p/facebook/9wzdncrfj2wl?cid=msft_web_appsforwindows_chart#activetab=pivot:overviewtab', 20, 1.00, 50.00, 'Mở nhanh ứng dụng gần đây'),
 (1, 3, 6, 'Splashtop', 'Ramus', 'Get rid of your navigation bar and learn to appreciate the benefits of gesture-based navigation. There was never before, a more natural way, to control your smartphone. Let the future begin!', 'splashtopicon.png', 'volumeslide4.png', 'recentbar4.png', 'edge4.jpg', 'https://www.microsoft.com/vi-vn/p/facebook/9wzdncrfj2wl?cid=msft_web_appsforwindows_chart#activetab=pivot:overviewtab', 15, 1.00, 50.00, 'Biến màn hình điện thoại thành màn hình phụ'),
 (1, 1, 7, 'Datally', 'Google', 'Get rid of your navigation bar and learn to appreciate the benefits of gesture-based navigation. There was never before, a more natural way, to control your smartphone. Let the future begin!', 'datallyicon.png', 'datally1.png', 'datally2.png', 'datally3.png', 'https://www.microsoft.com/vi-vn/p/facebook/9wzdncrfj2wl?cid=msft_web_appsforwindows_chart#activetab=pivot:overviewtab', 11, 1.00, 50.00, 'Theo dõi lưu lượng sử dụng'),
 (1, 1, 8, 'File go', 'Google', 'Get rid of your navigation bar and learn to appreciate the benefits of gesture-based navigation. There was never before, a more natural way, to control your smartphone. Let the future begin!', 'filesgoicon.png', 'filego1.png', 'filego2.png', 'filego3.png', 'https://www.microsoft.com/vi-vn/p/facebook/9wzdncrfj2wl?cid=msft_web_appsforwindows_chart#activetab=pivot:overviewtab', 95, 1.00, 50.00, 'Quan ly file'),
 (1, 1, 9, 'Just aline', 'Google', 'Get rid of your navigation bar and learn to appreciate the benefits of gesture-based navigation. There was never before, a more natural way, to control your smartphone. Let the future begin!', 'justalineicon.png', 'justaline1.png', 'justaline2.png', 'justaline3.png', 'https://www.microsoft.com/vi-vn/p/facebook/9wzdncrfj2wl?cid=msft_web_appsforwindows_chart#activetab=pivot:overviewtab', 95, 1.00, 50.00, 'Ứng dụng AR'),
-(1, 1, 10, 'Forest', 'Google', 'Get rid of your navigation bar and learn to appreciate the benefits of gesture-based navigation. There was never before, a more natural way, to control your smartphone. Let the future begin!', 'floatingbaricon.png', 'forestlive1.png', 'forestlive2.png', 'forestlive3.png', 'https://www.microsoft.com/vi-vn/p/facebook/9wzdncrfj2wl?cid=msft_web_appsforwindows_chart#activetab=pivot:overviewtab', 95, 1.00, 50.00, 'Ứng dụng AR'),
-(2, 14, 11, 'Monster', 'Sigma game', 'Get rid of your navigation bar and learn to appreciate the benefits of gesture-based navigation. There was never before, a more natural way, to control your smartphone. Let the future begin!', 'monstericon.png', 'monster1.jpg', 'monster2.jpg', 'monster3.jpg', 'https://www.microsoft.com/vi-vn/p/facebook/9wzdncrfj2wl?cid=msft_web_appsforwindows_chart#activetab=pivot:overviewtab', 32, 1.00, 50.00, 'Trò chơi nhập vai'),
-(2, 18, 12, 'Pokemon quest', 'Sigma game', 'Get rid of your navigation bar and learn to appreciate the benefits of gesture-based navigation. There was never before, a more natural way, to control your smartphone. Let the future begin!', 'pokemon1.png', 'pokemon1.png', 'pokemon2.png', 'pokemon3.png', 'https://www.microsoft.com/vi-vn/p/facebook/9wzdncrfj2wl?cid=msft_web_appsforwindows_chart#activetab=pivot:overviewtab', 65, 1.00, 50.00, 'Game giải trí nhẹ nhàng'),
-(2, 20, 13, 'Green farm', 'Gameloft', 'Get rid of your navigation bar and learn to appreciate the benefits of gesture-based navigation. There was never before, a more natural way, to control your smartphone. Let the future begin!', 'greenfarmlogo.png', 'greenfarm1.jpg', 'greenfarm2.jpg', 'greenfarm3.jpg', 'https://www.microsoft.com/vi-vn/p/facebook/9wzdncrfj2wl?cid=msft_web_appsforwindows_chart#activetab=pivot:overviewtab', 65, 1.00, 50.00, 'Game nông trại giải trí'),
+(1, 1, 10, 'Forest', 'Google', 'Get rid of your navigation bar and learn to appreciate the benefits of gesture-based navigation. There was never before, a more natural way, to control your smartphone. Let the future begin!', 'floatingbaricon.png', 'forestlive1.png', 'forestlive2.png', 'forestlive3.png', 'https://www.microsoft.com/vi-vn/p/facebook/9wzdncrfj2wl?cid=msft_web_appsforwindows_chart#activetab=pivot:overviewtab', 95, 1.00, 50.00, 'Ứng dụng AR');
+
 -- --------------------------------------------------------
 
 --
@@ -72,7 +68,7 @@ CREATE TABLE IF NOT EXISTS `Banners` (
   `IdBanner` int(10) unsigned NOT NULL,
   `ContentBanner` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `ImageBanner` varchar(100) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `Banners`
@@ -82,8 +78,7 @@ INSERT INTO `Banners` (`IdBanner`, `ContentBanner`, `ImageBanner`) VALUES
 (1, 'Nơi đây tổng hợp các phần mềm hoàn toàn miễn phí', 'banner page.jpg'),
 (2, 'Những trò chơi hay nhất đang đợi bạn', 'banner 2.jpg'),
 (3, 'Hãy chia sẽ trang này cho bạn bè của bạn', 'banner 3.jpg'),
-(4, 'Những ứng dụng mang đến trải nghiệm tốt nhất', 'banner 4.jpg'),
-
+(4, 'Những ứng dụng mang đến trải nghiệm tốt nhất', 'banner 4.jpg');
 
 -- --------------------------------------------------------
 
@@ -94,7 +89,7 @@ INSERT INTO `Banners` (`IdBanner`, `ContentBanner`, `ImageBanner`) VALUES
 CREATE TABLE IF NOT EXISTS `Category` (
   `IdCategory` int(10) unsigned NOT NULL,
   `NameCategory` varchar(50) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `Category`
@@ -102,31 +97,7 @@ CREATE TABLE IF NOT EXISTS `Category` (
 
 INSERT INTO `Category` (`IdCategory`, `NameCategory`) VALUES
 (1, 'Software'),
-(2, 'Game'),
-(3, 'Book'),
-
--- --------------------------------------------------------
-
---
--- Table structure for table `migrations`
---
-
-CREATE TABLE IF NOT EXISTS `migrations` (
-  `id` int(10) unsigned NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `migrations`
---
-
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2018_10_20_122613_create_category_table', 1),
-(2, '2018_10_20_173759_create_types_table', 1),
-(5, '2018_10_20_132909_create_application_table', 2),
-(6, '2018_10_20_142433_create_reviews_table', 2),
-(7, '2018_10_20_145632_create_banners_table', 2),
+(2, 'Book');
 
 -- --------------------------------------------------------
 
@@ -162,7 +133,7 @@ CREATE TABLE IF NOT EXISTS `Reviews` (
 CREATE TABLE IF NOT EXISTS `Types` (
   `IdType` int(10) unsigned NOT NULL,
   `NameType` varchar(30) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `Types`
@@ -192,9 +163,7 @@ INSERT INTO `Types` (`IdType`, `NameType`) VALUES
 (21, 'Cổ điển'),
 (22, 'Phiêu lưu'),
 (23, 'Bắn súng'),
-(24, 'Đố chữ'),
-(25, 'Nhập vai'),
-
+(24, 'Đố chữ');
 
 -- --------------------------------------------------------
 
@@ -229,10 +198,28 @@ INSERT INTO `users` (`id`, `level`, `name`, `email`, `email_verified_at`, `passw
 --
 
 --
--- Indexes for table `migrations`
+-- Indexes for table `Application`
 --
-ALTER TABLE `migrations`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `Application`
+  ADD PRIMARY KEY (`IdApplication`);
+
+--
+-- Indexes for table `Banners`
+--
+ALTER TABLE `Banners`
+  ADD PRIMARY KEY (`IdBanner`);
+
+--
+-- Indexes for table `Category`
+--
+ALTER TABLE `Category`
+  ADD PRIMARY KEY (`IdCategory`);
+
+--
+-- Indexes for table `Types`
+--
+ALTER TABLE `Types`
+  ADD PRIMARY KEY (`IdType`);
 
 --
 -- Indexes for table `users`
@@ -245,10 +232,25 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `migrations`
+-- AUTO_INCREMENT for table `Application`
 --
-ALTER TABLE `migrations`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=29;
+ALTER TABLE `Application`
+  MODIFY `IdApplication` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `Banners`
+--
+ALTER TABLE `Banners`
+  MODIFY `IdBanner` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `Category`
+--
+ALTER TABLE `Category`
+  MODIFY `IdCategory` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `Types`
+--
+ALTER TABLE `Types`
+  MODIFY `IdType` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=25;
 --
 -- AUTO_INCREMENT for table `users`
 --
